f255ddaefd6b3442ea359ceff4d1ff6aa2ceea15
Jesus Garcia <jesusgarcia@Kubo-MacBook.local>
2022-02-26 17:19:32 -0600

    proyect was clenaned

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Component from "@/components/pages/DesignSystem/";
-import './styles/main.scss'
-
-ReactDOM.render(<Component />, document.getElementById("app"));
