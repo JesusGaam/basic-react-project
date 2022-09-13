8da5aa0868eb348ec01d48d84efa47df6a1ecd97
Jesus Garcia <jesusgarcia@Kubo-MacBook-2.local>
2022-09-13 17:38:36 -0500

    mpa sass mobx configuration was implemented

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "../src/styles/main.scss";
+import Component from "{component}";
+
+ReactDOM.render(<Component />, document.getElementById("app"));
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,4 +1,35 @@
-export { default as DesignSystem } from "./designSystem/DesignSystem";
-export { default as EmitProps } from "./emitProps/EmitProps";
-export { default as MobxSPA } from "./mobxSPA/MobxSPA";
-export { default as NamesMobx } from "./namesMobx/NamesMobx";
+module.exports.metadata = {
+  title: "React MPA page",
+  description: "Web page optimized",
+  keywords: "react MPA",
+  image: "",
+  robots: true,
+  canonical: "",
+  schema: {},
+  priority: "1",
+  lastmod: "",
+  enable: true,
+};
+
+module.exports.pages = [
+  {
+    filename: "index.html",
+    component: "@/components/pages/DesignSystem/DesignSystem.js",
+    title: "Design System",
+  },
+  {
+    filename: "emit-props.html",
+    component: "@/components/pages/emitProps/EmitProps.js",
+    title: "Emit Props",
+  },
+  {
+    filename: "mobx-spa.html",
+    component: "@/components/pages/mobxSPA/MobxSPA.js",
+    title: "Mobx SPA",
+  },
+  {
+    filename: "names-mobx.html",
+    component: "@/components/pages/namesMobx/NamesMobx.js",
+    title: "Mobx Names",
+  },
+];
